0a341d44be327f26567453bbe875849a2a87952b
kyungs <83746849+kskim625@users.noreply.github.com>
2022-05-07 21:45:48 +0900

    Update 큰수만들기.js

--- JavaScript/큰수만들기.js
+++ JavaScript/큰수만들기.js
@@ -1,27 +1,14 @@
-function solution(citations) {
-    const maxCitations = citations[citations.length - 1];
-    let hIndex = 0;
-    while (hIndex <= maxCitations) {
-        let mentioned = 0, notMentioned = 0;
-        for (let i = 0; i < citations.length; i++) {
-            if (citations[i] >= hIndex) mentioned += 1;
-            else notMentioned += 1;
+//unsolved(효율성)
+function solution(number, k) {    
+    let removed = 0, idx = 0;
+    while (removed < k && idx < number.length - 1) {
+        if (number[idx] < number[idx + 1]) {
+            number = number.substring(0, idx) + number.substring(idx + 1, number.length);
+            removed += 1;
+            idx -= idx > 0 ? 1 : 0;
+            continue;
         }
-        if (mentioned > hIndex && mentioned > notMentioned) {
-            hIndex += 1;
-        } else {
-            if (mentioned < hIndex || mentioned < notMentioned) {
-                hIndex -= 1;
-            }
-            break;
-        }
+        idx += 1;
     }
-    return hIndex;
+    return removed < k ? number.substring(0, number.length - k) : number;
 }
-
-console.log(solution([3, 0, 6, 1, 5]));
-console.log(solution([22, 42]));
-// console.log(solution([1, 1, 1]));
-// console.log(solution([0]));
-console.log(solution([1, 4]));
-console.log(solution([0, 1, 2]));
