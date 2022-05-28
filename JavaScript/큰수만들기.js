f2fe8cad431ba6a324a5e35b7cfe6031b1ebe2ba
kyungs <83746849+kskim625@users.noreply.github.com>
2022-05-28 22:21:09 +0900

    Add files via upload

--- JavaScript/큰수만들기.js
+++ JavaScript/큰수만들기.js
@@ -1,15 +1,18 @@
-function solution(number, k) {
-    let removed = 0;
-    const stack = [number[0]];
-    for (let i = 1; i < number.length; i++) {
-        let idx = stack.length - 1;
-        while (stack[idx] < number[i] && idx >= 0 && removed < k) {
-            stack.pop();
+function solution(number, k) {    
+    let removed = 0, idx = 0;
+    while (removed < k && idx < number.length - 1) {
+        if (number[idx] < number[idx + 1]) {
+            number = number.substring(0, idx) + number.substring(idx + 1, number.length);
             removed += 1;
-            idx -= 1;
+            idx -= idx > 0 ? 1 : 0;
+            continue;
         }
-        stack.push(number[i]);
+        idx += 1;
     }
-    const answer = stack.join('');
-    return removed === k ? answer : answer.substring(0, answer.length - k);
+    return removed < k ? number.substring(0, number.length - k) : number;
 }
+
+console.log(solution("1924", 2));
+console.log(solution("1231234", 3));
+console.log(solution("4177252841", 4));
+console.log(solution("8888889", 6));
