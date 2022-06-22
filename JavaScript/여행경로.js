477c3d97416977013cfd70db2f809b5a37342dc6
kyungs <83746849+kskim625@users.noreply.github.com>
2022-06-22 22:07:46 +0900

    Add files via upload

--- JavaScript/여행경로.js
+++ JavaScript/여행경로.js
@@ -1,30 +1,41 @@
-const getOrder = (tickets, currentLocation, visited, order, answer) => {
-    if (order.length - 1 === tickets.length) return;
-    
-    for (let i = 0; i < tickets.length; i++) {
-        if (visited.includes(i)) continue;
-        if (tickets[i][0] === currentLocation) {
-            const v = JSON.parse(JSON.stringify(visited));
-            const o = JSON.parse(JSON.stringify(order));
-            v.push(i);
-            o.push(currentLocation);
-            if (tickets.length === o.length) {
-                o.push(tickets[i][1]);
-                answer.push(o)
-                break;
-            }
-            getOrder(tickets, tickets[i][1], v, o, answer);
-        }
-    }
-}
+// unsolved
 
 function solution(tickets) {
-    const startLocation = 'ICN';
-    const answer = [];
+    var answer = [];
+
+    tickets.sort();
+    let ticket;
     for (let i = 0; i < tickets.length; i++) {
-        if (tickets[i][0] === startLocation) {
-            getOrder(tickets, tickets[i][1], [i], [startLocation], answer);
+        if (tickets[i][0] === 'ICN') {
+            ticket = tickets.slice();
+            ticket.splice(i, 1);
+            answer.push(dfs(['ICN'], tickets.slice(), ticket, tickets[i]));
         }
     }
-    return answer.sort()[0];
+    console.log(answer);
+    answer.sort();
+    for (let j = 0; j < answer.length; j++) if (answer[j].length === tickets.length + 1) return answer[j];
 }
+
+function dfs(answer, tickets, ticket, current) {
+    if (ticket.length === 0) {
+        return answer.push(current[1]);
+    }
+
+    let temp = [];
+    for (let i = 0; i < ticket.length; i++) {
+        let t = [];
+        if (current[1] === ticket[i][0]){
+            answer.push(current[1]);
+            for (let j = 0; j < ticket.length; j++) if (j !== i) t.push(ticket[j]);
+            temp = dfs(answer, tickets, t, ticket[i]);
+        }
+        if (temp.length === tickets.length + 1) return temp;
+    }
+    console.log(temp, answer)
+    return temp;
+}
+
+//console.log(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]));
+console.log(solution([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]));
+ //                          x                x               x               x                 x               x                x          
